17a869bbc2284f24a5811ce32a16455061e716d1
ricky <whdtjr2792@gmail.com>
2020-09-26 09:12:48 +0900

    ...

--- src/openchat/openchat.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { OpenchatResolver } from './openchat.resolver';
-import { OpenchatService } from './openchat.service';
-
-@Module({
-  providers: [OpenchatResolver, OpenchatService]
-})
-export class OpenchatModule {}
